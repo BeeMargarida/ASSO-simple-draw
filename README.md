568e6e9b60e883d854d613935879f98d112bc183
Hugo Sereno Ferreira <bytter@gmail.com>
2019-03-11 13:29:04 +0000

    Project skeleton

---
+++ README.md
@@ -0,0 +1,5 @@
+# SimpleDrawTS
+
+Typescript Browser-only SimpleDraw.
+
+ASSO / FEUP 2018/2019
---
+++ src/script.ts
@@ -0,0 +1,3 @@
+const canvas = <HTMLCanvasElement>document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
