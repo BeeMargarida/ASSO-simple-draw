1573a7076eec6d8530d45bc1d7b581af7c702dab
Hugo Sereno Ferreira <bytter@gmail.com>
2019-03-11 18:19:00 +0000

    Support multiple renders.

--- src/script.ts
+++ src/script.ts
@@ -1,20 +1,16 @@
 import { SimpleDrawDocument } from './document'
+import { CanvasRender, SVGRender } from './render';
 
-const canvas = <HTMLCanvasElement>document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
+const canvasrender = new CanvasRender()
+const svgrender = new SVGRender()
 
 const sdd = new SimpleDrawDocument()
 const c1 = sdd.createCircle(50, 50, 30)
-sdd.translate(c1, 50, 50)
 const r1 = sdd.createRectangle(10, 10, 80, 80)
-sdd.undo()
-sdd.undo()
-sdd.undo()
-sdd.undo()
-const r2 = sdd.createRectangle(10, 10, 80, 80)
-sdd.undo()
-sdd.redo()
-sdd.redo()
+const r2 = sdd.createRectangle(30, 30, 40, 40)
 
-sdd.draw(ctx)
+/* const s1 = sdd.createSelection(c1, r1, r2)
+sdd.translate(s1, 10, 10) */
 
+sdd.draw(canvasrender)
+sdd.draw(svgrender)
