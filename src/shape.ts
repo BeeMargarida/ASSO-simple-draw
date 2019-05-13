caec62e13336c189d7f88f43302a297308e48b73
up201505509 <up201505509@fe.up.pt>
2019-05-13 15:42:37 +0100

    Fix selection of objects

--- src/shape.ts
+++ src/shape.ts
@@ -3,11 +3,16 @@ import { getCoordWithZoom } from "./utils";
 
 export abstract class Shape {
     color: string
+    centerX: number
+    centerY: number
     constructor(public x: number, public y: number) { }
+
+    abstract updateCenter() : void
 
     translate(xd: number, yd: number): void {
         this.x += xd
         this.y += yd
+        this.updateCenter()
     }
 
     abstract checkIfHit(mx: number, my: number, render: CanvasRender | SVGRender) : boolean
@@ -24,6 +29,11 @@ export class Rectangle extends Shape {
         this.color = 'black'
     }
 
+    updateCenter() {
+        this.centerX = this.x + this.width / 2
+        this.centerY = this.y + this.height / 2
+    }
+
     checkIfHit(mx: number, my: number, render: CanvasRender | SVGRender): boolean {
         var sx = getCoordWithZoom(this.x, render.centerX, render.zoom)
         var sy = getCoordWithZoom(this.y, render.centerY, render.zoom)
@@ -37,6 +47,13 @@ export class Circle extends Shape {
     constructor(public x: number, public y: number, public radius: number) {
         super(x, y)
         this.color = 'black'
+        this.centerX = x
+        this.centerY = y
+    }
+
+    updateCenter() {
+        this.centerX = this.x
+        this.centerY = this.y
     }
     
     checkIfHit(mx: number, my: number, render: CanvasRender | SVGRender): boolean {
